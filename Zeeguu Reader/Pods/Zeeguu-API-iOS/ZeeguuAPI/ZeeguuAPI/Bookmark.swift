db6789c84edbbf7a1cc21ce7eb45e2da3b37c94e
Jorrit Oosterhof <jorrito@icloud.com>
2016-06-11 16:11:48 +0200

    Update pods

--- Zeeguu Reader/Pods/Zeeguu-API-iOS/ZeeguuAPI/ZeeguuAPI/Bookmark.swift
+++ Zeeguu Reader/Pods/Zeeguu-API-iOS/ZeeguuAPI/ZeeguuAPI/Bookmark.swift
@@ -49,6 +49,9 @@ public class Bookmark {
 	
 	// MARK: Properties -
 	
+	/// The id of this bookmark
+	public var id: String
+	
 	/// The date of this bookmark
 	public var date: NSDate
 	
@@ -74,6 +77,7 @@ public class Bookmark {
 	/**
 	Construct a new `Bookmark` object.
 	
+	- parameter id: The id of this bookmark
 	- parameter title: The title of the article in which `word` was translated
 	- parameter context: The context in which `word` was translated
 	- parameter url: The url of the article in which `word` was translated
@@ -83,7 +87,8 @@ public class Bookmark {
 	- parameter translation: The translation(s)
 	- parameter translationLanguage: The language of `translation`
 	*/
-	public init(title: String, context: String? = nil, url: String, bookmarkDate: String, word: String, wordLanguage: String, translation: [String], translationLanguage: String) {
+	public init(id: String, title: String, context: String? = nil, url: String, bookmarkDate: String, word: String, wordLanguage: String, translation: [String], translationLanguage: String) {
+		self.id = id
 		self.title = title
 		self.context = context
 		self.url = url
@@ -100,6 +105,44 @@ public class Bookmark {
 
 		let date = formatter.dateFromString(bookmarkDate)
 		self.date = date!
+	}
+	
+	/// Deletes this bookmark.
+	///
+	/// - parameter completion: A block that will receive a boolean indicating if the bookmark could be deleted or not.
+	public func delete(completion: (success: Bool) -> Void) {
+		ZeeguuAPI.sharedAPI().deleteBookmarkWithID(self.id) { (success) in
+			completion(success: success)
+		}
+	}
+	
+	/// Adds a translation to this bookmark.
+	///
+	/// - parameter translation: The translation to add to this bookmark.
+	/// - parameter completion: A block that will receive a boolean indicating if the translation could be added or not.
+	public func addTranslation(translation: String, completion: (success: Bool) -> Void) {
+		ZeeguuAPI.sharedAPI().addNewTranslationToBookmarkWithID(self.id, translation: translation) { (success) in
+			completion(success: success)
+		}
+	}
+	
+	/// Deletes a translation from this bookmark.
+	///
+	/// - parameter translation: The translation to remove from this bookmark.
+	/// - parameter completion: A block that will receive a boolean indicating if the translation could be deleted or not.
+	public func deleteTranslation(translation: String, completion: (success: Bool) -> Void) {
+		ZeeguuAPI.sharedAPI().deleteTranslationFromBookmarkWithID(self.id, translation: translation) { (success) in
+			completion(success: success)
+		}
+	}
+	
+	/// Retrieves all translations for this bookmark.
+	///
+	/// - parameter completion: A block that will receive a dictionary with the translations.
+	public func getTranslations(completion: (dict: JSON?) -> Void) {
+		ZeeguuAPI.sharedAPI().getTranslationsForBookmarkWithID(self.id) { (dict) in
+			completion(dict: dict)
+		}
 	}
 	
 }
